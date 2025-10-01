4765a7aabc5273f41394dfcee9a3a26d35243c6e
Macarenamoreyra <macarenamoreyra06@gmail.com>
2025-10-01 20:44:47 -0300

    Agregar archivos de proyecto.

---
+++ Clasesejer1_10/Data/AplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Clasesejer1_10.Data
+{
+    public class AplicationDbContext : DbContext
+
+    {
+        public DbSet<Vehículo> vehiculos{ get; set; }
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=PruebaEF;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}
---
+++ Clasesejer1_10/modelo/Vehículo.cs
@@ -0,0 +1,11 @@
+﻿namespace Clasesejer1_10
+{
+    public class Vehículo
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+
+    }
+}
